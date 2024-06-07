feb549dd1c8ae3250667217c5ad9ac637d5626a8
ManhVT <manhvt89@gmail.com>
2024-06-07 21:41:07 +0700

    o

--- application/views/recipes/form.php
+++ application/views/recipes/form.php
@@ -6,7 +6,18 @@
 	<fieldset id="item_basic_info">
 		<div class="form-group form-group-sm">
 			<?php echo form_label($this->lang->line('items_item_number'), 'item_number', array('class'=>'control-label col-xs-3')); ?>
-			<div class='col-xs-8'>
+			<div class='col-xs-6'>
+				<div class="input-group">
+					<span class="input-group-addon input-sm"><span class="glyphicon glyphicon-barcode"></span></span>
+					<?php echo form_input(array(
+							'name'=>'item_number',
+							'id'=>'item_number',
+							'class'=>'form-control input-sm',
+							'value'=>$item_info->item_number)
+							);?>
+				</div>
+			</div>
+			<div class='col-xs-6'>
 				<div class="input-group">
 					<span class="input-group-addon input-sm"><span class="glyphicon glyphicon-barcode"></span></span>
 					<?php echo form_input(array(

5cbdb691148713ff0541098bcdd57e2431b6570e
ManhVT <manhvt89@gmail.com>
2024-06-07 22:28:35 +0700

    o

--- application/views/recipes/form.php
+++ application/views/recipes/form.php
@@ -30,34 +30,34 @@
 			</div>
 			<div class='col-xs-8'>
 				<div class='col-xs-6'>
-					<div class='col-xs-4'>
+					<div class='col-xs-6'>
 						<?=$this->lang->line('recipes_master_batch')?>:
 					</div>
-					<div class='col-xs-8'>
+					<div class='col-xs-6'>
 						<b><?=$item_info->name?></b>
 					</div>
 				</div>
 				<div class='col-xs-6'>
-					<div class='col-xs-4'>
+					<div class='col-xs-6'>
 						<?=$this->lang->line('recipes_grade_of_standard')?>:
 					</div>
-					<div class='col-xs-8'>
+					<div class='col-xs-6'>
 						<b><?=$item_info->grade_of_standard?></b>
 					</div>
 				</div>
 				<div class='col-xs-6'>
-					<div class='col-xs-4'>
+					<div class='col-xs-6'>
 						<?=$this->lang->line('recipes_date_issued')?>:
 					</div>
-					<div class='col-xs-8'>
+					<div class='col-xs-6'>
 						<b><?=date('d/m/Y',$item_info->date_issued)?></b>
 					</div>
 				</div>
 				<div class='col-xs-6'>
-					<div class='col-xs-4'>
+					<div class='col-xs-6'>
 						<?=$this->lang->line('recipes_certificate_no')?>:
 					</div>
-					<div class='col-xs-8'>
+					<div class='col-xs-6'>
 						<b><?=$item_info->certificate_no?></b>
 					</div>
 				</div>

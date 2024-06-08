7de360407f279a0ff32f14c2ca5bf33bfe5598ac
ManhVT <manhvt89@gmail.com>
2024-06-08 11:00:01 +0700

    ok

--- application/views/recipes/form.php
+++ application/views/recipes/form.php
@@ -28,24 +28,47 @@
 
 	.recipe-header-body-kneader-a td:first-child {
 		width: 10%;
-		text-align: center;
+		
 	}
 	.recipe-header-body-kneader-a td:nth-child(2) {
 		width: 20%;
 	}
 	.recipe-header-body-kneader-a td:nth-child(3) {
 		width: 15%;
-		text-align: center;
+		
 	}
 	.recipe-header-body-kneader-a td:nth-child(4) {
 		width: 15%;
-		text-align: right;
+		
 	}
 	.recipe-header-body-kneader-a td:nth-child(5) {
 		width: 15%;
-		text-align: right;
+		
 	}
 	.recipe-header-body-kneader-a td:nth-child(6) {
+		width: 25%;
+	}
+
+	.recipe-item-body-kneader-a td:first-child {
+		width: 10%;
+		text-align: center;
+	}
+	.recipe-item-body-kneader-a td:nth-child(2) {
+		width: 20%;
+	}
+	.recipe-item-body-kneader-a td:nth-child(3) {
+		width: 15%;
+		text-align: center;
+	}
+	.recipe-item-body-kneader-a td:nth-child(4) {
+		width: 15%;
+		text-align: right;
+	}
+	.recipe-item-body-kneader-a td:nth-child(5) {
+		width: 15%;
+		text-align: right;
+	}
+	.recipe-item-body-kneader-a td:nth-child(6) {
 		width: 25%;
 		text-align: center;
 	}
@@ -56,24 +79,48 @@
 
 	.recipe-header-body-kneader-b td:first-child {
 		width: 10%;
-		text-align: center;
+		
 	}
 	.recipe-header-body-kneader-b td:nth-child(2) {
 		width: 20%;
 	}
 	.recipe-header-body-kneader-b td:nth-child(3) {
 		width: 15%;
-		text-align: center;
+		
 	}
 	.recipe-header-body-kneader-b td:nth-child(4) {
 		width: 15%;
-		text-align: right;
+		
 	}
 	.recipe-header-body-kneader-b td:nth-child(5) {
 		width: 15%;
-		text-align: right;
+		
 	}
 	.recipe-header-body-kneader-b td:nth-child(6) {
+		width: 25%;
+		
+	}
+
+	.recipe-item-body-kneader-b td:first-child {
+		width: 10%;
+		text-align: center;
+	}
+	.recipe-item-body-kneader-b td:nth-child(2) {
+		width: 20%;
+	}
+	.recipe-item-body-kneader-b td:nth-child(3) {
+		width: 15%;
+		text-align: center;
+	}
+	.recipe-item-body-kneader-b td:nth-child(4) {
+		width: 15%;
+		text-align: right;
+	}
+	.recipe-item-body-kneader-b td:nth-child(5) {
+		width: 15%;
+		text-align: right;
+	}
+	.recipe-item-body-kneader-b td:nth-child(6) {
 		width: 25%;
 		text-align: center;
 	}
